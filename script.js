731dd32d1106b38a8d240ba98fcc56f9c09783c4
maxjeanty <maxjeanty@gmail.com>
2023-10-26 00:41:41 -0500

    working on search JS code,, corrected typo

---
+++ script.js
@@ -0,0 +1,36 @@
+const search = document.getElementById('search'),
+    submit=document.getElementById('submit'),
+    random = document.getElementById('random'),
+    mealsEl = document.getElementById('meals')
+    resultHeading = document.getElementById('result-heading')
+    single_mealEl = document.getElementById('single-meal')
+
+
+//search meal and fetch api
+function searchMeal(e) {
+    e.preventDefault();
+  
+    // Clear single meal
+    single_mealEl.innerHTML = '';
+  
+    // Get search term
+    const term = search.value;
+    // check for empty
+    if (term.trim()) {
+      fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+        .then(res => res.json())
+        .then(data =>{
+        console.log(data)
+        resultHeading.innerHTML =`<h2>Search Results for '${term}':</h2>`
+        })
+    } else {
+      alert('please put in a value')  
+
+    }
+}
+
+
+
+//event listeners
+// the . in the submit button adds an even listener to it
+submit.addEventListener('submit', searchMeal)
